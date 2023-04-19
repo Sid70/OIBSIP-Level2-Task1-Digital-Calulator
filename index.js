f677dfa4f0da6edcb779fd7a0570b7d054962543
Siddhanta Choudhury <103874320+Sid70@users.noreply.github.com>
2023-04-19 14:00:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-const  clr = () => document.getElementById('result').value = " ";
+const clr = () => document.getElementById('result').value = " ";
 
 
 const display = (val) => {
     document.getElementById('result').value += val;
 }
 
-const del = () => document.getElementById('result').value = (document.getElementById('result').value).slice(0,-1);
+const del = () => document.getElementById('result').value = (document.getElementById('result').value).slice(0, -1);
 
 let calculate = () => {
-    let x = document.getElementById('result').value;
-    let y = eval(x);
-    document.getElementById('result').value = y;
+    try {
+        let x = document.getElementById('result').value;
+        let y = eval(x);
+        document.getElementById('result').value = y;
+    } catch (error) {
+        alert('Please Enter a Valid Expression.');
+        console.log(error);
+    }
 }
